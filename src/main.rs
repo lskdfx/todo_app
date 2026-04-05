e5a4e08c1e689082856192b1ec7a76eedbeb0338
Adit Gupta <guptaditz@gmail.com>
2026-04-04 17:19:45 -0700

    Base todo app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+use eframe::egui::Ui;
+struct TodoApp {}
+
+
+impl TodoApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self {}
+    }
+
+}
+
+impl eframe::App for TodoApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My To-Do List");
+        });
+    }
+    fn ui(&mut self, ui: &mut Ui, _frame: &mut eframe::Frame) {}
+}
+
+fn main() -> eframe::Result {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "To-Do List",
+        native_options,
+        Box::new(|cc| Ok(Box::new(TodoApp::new(cc)))),
+    )
 }
